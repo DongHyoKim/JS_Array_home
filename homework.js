fd96206ef7ec80ad6e9288510833c59b45353a15
DongHyo Kim <kmkimdh@makebot.ai>
2020-06-04 15:22:37 +0900

    숙제완료ㅋㅋ

--- homework.js
+++ homework.js
@@ -1,16 +1,26 @@
 // 1번 숙제
 let votes = ["kim", "lee", "lee", "park", "park", "lee", "kim"];
-let resultkim = votes.filter(name => name === 'kim');
-let resultlee = votes.filter(name => name === 'lee');
-let resultpark = votes.filter(name => name === 'park');
-// { kim 2, lee 3, park 2 }
-console.log("{ kim " + resultkim.length + ", lee " + resultlee.length + ", park " + resultpark.length + "}");
+// let resultkim = votes.filter(name => name === 'kim');
+// let resultlee = votes.filter(name => name === 'lee');
+// let resultpark = votes.filter(name => name === 'park');
+// // { kim 2, lee 3, park 2 }
+// console.log("{ kim " + resultkim.length + ", lee " + resultlee.length + ", park " + resultpark.length + "}");
+let result1 = {};
+votes.forEach(entity => {
+    if (!result1[entity]) {
+        result1[entity] = 1;
+    } else {
+        result1[entity]++;
+    };
+});
+console.log(result1);
+
 
 // 2번 숙제
 let numbers = [1, 2, 3, 4, 5];
 // 메소드 하나만 사용하여  다음 결과를 만드세요
-let result = numbers.map(function (x) {
+let result2 = numbers.map(function (x) {
     return x * 2
 });
 // { 2, 4, 6, 8, 10 }
-console.log(result);
+console.log(result2);
